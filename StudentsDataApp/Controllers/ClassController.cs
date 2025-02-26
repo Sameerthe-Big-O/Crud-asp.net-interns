f1f54590624422e8dab7a2be9e634c1537e0c677
MHamzaJavaid <160497304+MHamzaJavaid@users.noreply.github.com>
2025-02-26 23:35:47 +0500

    update

--- StudentsDataApp/Controllers/ClassController.cs
+++ StudentsDataApp/Controllers/ClassController.cs
@@ -1,9 +1,57 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Security.Cryptography.X509Certificates;
+using Microsoft.AspNetCore.Mvc;
+using StudentsDataApp.DAL;
+using StudentsDataApp.Models;
 
 namespace StudentsDataApp.Controllers
 {
     public class ClassController : Controller
     {
+        private readonly ClassDataAccessLayer dal;
+
+        public ClassController()
+        {
+            dal = new ClassDataAccessLayer();
+        }
+
+       
+        [Obsolete]
+        public ActionResult GetAllClasses()
+        {
+            List<Class> cls = dal.GetAllClasses();
+            return View(cls);
+            
+        }
+
+       
+        [Obsolete]
+        public JsonResult create(Class cls)
+        {
+            dal.AddClass(cls);
+            return Json(new { success = true, message = "Class added successfully!" });
+        }
+
+        [Obsolete]
+        public ActionResult edit(int id)
+        {
+            Class cls = dal.GetClassById(id);
+            return View(cls);
+        }
+        [HttpPost]
+        [Obsolete]
+        public JsonResult edit(Class cls)
+        {
+            dal.UpdateClass(cls);
+            return Json(new { success = true, message = "Class updated successfully!" });
+        }
+
+        [HttpPost]
+        
+        public JsonResult delete(int id)
+        {
+            dal.DeleteClass(id);
+            return Json(new { success = true, message = "Class deleted successfully!" });
+        }
         public IActionResult Index()
         {
             return View();
--- StudentsDataApp/DAL/ClassDataAccessLayer.cs
+++ StudentsDataApp/DAL/ClassDataAccessLayer.cs
@@ -1,6 +1,107 @@
-﻿namespace StudentsDataApp.DAL
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Xml.Serialization;
+using StudentsDataApp.Models;
+
+namespace StudentsDataApp.DAL
 {
     public class ClassDataAccessLayer
     {
+        string cs = ConnectionString.dbcs;
+
+        [Obsolete]
+        public List<Class> GetAllClasses()
+        {
+            List<Class> classlist = new List<Class>();
+
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("spGetAllClasses", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Class cls = new Class
+                    {
+                        Class_ID = Convert.ToInt32(reader["class_id"]),
+                        Class_Name = reader["class_name"].ToString()
+                    };
+                    classlist.Add(cls);
+                }
+
+            }
+            return classlist;
+        }
+
+        [Obsolete]
+        public Class GetClassById(int id)
+        {
+            Class cls = null;
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("spGetClassById", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("class_id", id);
+                con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    cls = new Class
+                    {
+                        Class_ID = Convert.ToInt32(reader["class_id"]),
+                        Class_Name = reader["class_name"].ToString()
+                    };
+                }
+            }
+            return cls;
+        }
+
+        [Obsolete]
+        public void AddClass(Class cls)
+        {
+            using(SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("spAddClass", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@class_name" , cls.Class_Name);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        [Obsolete]
+        public void UpdateClass(Class cls)
+        {
+            using(SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("spUpdateClass", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@class_id", cls.Class_ID);
+                cmd.Parameters.AddWithValue("@class_name", cls.Class_Name);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void DeleteClass(int id)
+        {
+            using(SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("spDeleteClass", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@class_id", id);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
     }
 }
